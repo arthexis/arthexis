49b71e033366651b212d00e17820137ccb41f1c9
Rafael Guillen <arthexis@gmail.com>
2025-07-28 21:43:06 -0600

    Show websocket URLs at server startup

---
+++ readme/management/commands/runserver.py
@@ -0,0 +1,16 @@
+from django.core.management.commands.runserver import Command as RunserverCommand
+
+class Command(RunserverCommand):
+    """Extended runserver command that also prints WebSocket URLs."""
+
+    def on_bind(self, server_port):
+        super().on_bind(server_port)
+        host = self.addr or (self.default_addr_ipv6 if self.use_ipv6 else self.default_addr)
+        scheme = 'wss' if self.ssl_options else 'ws'
+        # Display available websocket URLs.
+        websocket_paths = ['/ws/echo/', '/ws/ocpp/<cid>/']
+        for path in websocket_paths:
+            self.stdout.write(f"WebSocket available at {scheme}://{host}:{server_port}{path}")
+
+
+
