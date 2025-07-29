2ed7363cc1497aab2a03bca11ab765f3cd4a881b
Rafael Guillen <arthexis@gmail.com>
2025-07-28 19:57:05 -0600

    Add simulator model with admin controls

--- ocpp/admin.py
+++ ocpp/admin.py
@@ -1,9 +1,47 @@
 from django.contrib import admin
 
-from .models import Charger
+import asyncio
+
+from .models import Charger, Simulator
+from .simulator import ChargePointSimulator
+from . import store
 
 
 @admin.register(Charger)
 class ChargerAdmin(admin.ModelAdmin):
     list_display = ("charger_id", "name", "require_rfid", "last_heartbeat")
     search_fields = ("charger_id", "name")
+
+
+@admin.register(Simulator)
+class SimulatorAdmin(admin.ModelAdmin):
+    list_display = ("name", "cp_path", "host", "ws_port", "running")
+    actions = ("start_simulator", "stop_simulator")
+
+    def running(self, obj):
+        return obj.pk in store.simulators
+
+    running.boolean = True
+
+    def start_simulator(self, request, queryset):
+        for obj in queryset:
+            if obj.pk in store.simulators:
+                continue
+            sim = ChargePointSimulator(obj.as_config())
+            sim.start()
+            store.simulators[obj.pk] = sim
+        self.message_user(request, "Simulators started")
+
+    start_simulator.short_description = "Start selected simulators"
+
+    def stop_simulator(self, request, queryset):
+        async def _stop(objs):
+            for obj in objs:
+                sim = store.simulators.pop(obj.pk, None)
+                if sim:
+                    await sim.stop()
+
+        asyncio.get_event_loop().create_task(_stop(list(queryset)))
+        self.message_user(request, "Stopping simulators")
+
+    stop_simulator.short_description = "Stop selected simulators"
