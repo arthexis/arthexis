fd9b559484a1136556eda077bbe9d407f9b964ac
Rafael Guillen <arthexis@gmail.com>
2025-07-29 11:46:27 -0600

    Improve navigation bar styling and cleanup

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = "website"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("sitemap.xml", views.sitemap, name="website-sitemap"),
 ]
