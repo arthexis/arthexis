18a16161d31ec13ed46b4cf7f7674177db2e620d
Rafael Guillen <arthexis@gmail.com>
2025-08-06 21:11:11 -0600

    feat: track requirements checksum

--- dev-maintenance.bat
+++ dev-maintenance.bat
@@ -8,7 +8,16 @@ if not exist %VENV%\Scripts\python.exe (
     exit /b 0
 )
 if exist requirements.txt (
-    %VENV%\Scripts\python.exe -m pip install -r requirements.txt
+    for /f "skip=1 tokens=1" %%h in ('certutil -hashfile requirements.txt MD5') do (
+        if not defined REQ_HASH set REQ_HASH=%%h
+    )
+    if exist requirements.md5 (
+        set /p STORED_HASH=<requirements.md5
+    )
+    if /I not "%REQ_HASH%"=="%STORED_HASH%" (
+        %VENV%\Scripts\python.exe -m pip install -r requirements.txt
+        echo %REQ_HASH%>requirements.md5
+    )
 )
 set "DB_FILE=%VENV%\..\db.sqlite3"
 if defined DB_PATH set "DB_FILE=%DB_PATH%"
