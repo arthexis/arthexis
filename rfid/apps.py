1e0fc27269f1eda73916d26871ad21fdff392e9f
Rafael Guillen <arthexis@gmail.com>
2025-08-08 18:45:30 -0600

    chore: drop gpiozero and mfrc522 dependencies

--- rfid/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RFIDConfig(AppConfig):
-    """Configuration for the RFID app."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "rfid"
-    verbose_name = "RFID"
