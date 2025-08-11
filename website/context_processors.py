9217819f43337c29112efd70a522b0f976401262
Rafael Guillen <arthexis@gmail.com>
2025-08-11 15:19:10 -0600

    Hide apps without URLs from navbar

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,4 +1,5 @@
 from django.contrib.sites.shortcuts import get_current_site
+from django.urls import Resolver404, resolve
 
 
 def nav_links(request):
@@ -8,4 +9,13 @@ def nav_links(request):
         applications = site.site_applications.select_related("application").all()
     except Exception:
         applications = []
-    return {"nav_apps": applications}
+
+    valid_apps = []
+    for app in applications:
+        try:
+            resolve(app.path)
+        except Resolver404:
+            continue
+        valid_apps.append(app)
+
+    return {"nav_apps": valid_apps}
