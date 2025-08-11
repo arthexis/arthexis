7d45f05bd2a5f04533b88df0ba7274f7b67d16b5
Rafael Guillen <arthexis@gmail.com>
2025-08-11 15:38:25 -0600

    Improve app index view docs

--- nodes/urls.py
+++ nodes/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from website.views import app_index
+
 from . import views
 
 urlpatterns = [
-    path('list/', views.node_list, name='node-list'),
-    path('register/', views.register_node, name='register-node'),
-    path('screenshot/', views.capture, name='node-screenshot'),
-    path('<slug:endpoint>/', views.public_node_endpoint, name='node-public-endpoint'),
+    path("", app_index, {"module": __name__}, name="index"),
+    path("list/", views.node_list, name="node-list"),
+    path("register/", views.register_node, name="register-node"),
+    path("screenshot/", views.capture, name="node-screenshot"),
+    path("<slug:endpoint>/", views.public_node_endpoint, name="node-public-endpoint"),
 ]
