dbf0ae100ecc5dddcfdc69e0364baeeb4a2c3f6a
Rafael Guillen <arthexis@gmail.com>
2025-08-29 21:32:34 -0600

    Add migration for reference transaction UUID

---
+++ core/migrations/0005_reference_transaction_uuid.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+import uuid
+
+
+def assign_transaction_uuids(apps, schema_editor):
+    Reference = apps.get_model("core", "Reference")
+    for ref in Reference.objects.all():
+        ref.transaction_uuid = uuid.uuid4()
+        ref.save(update_fields=["transaction_uuid"])
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0004_userdatum"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="reference",
+            name="transaction_uuid",
+            field=models.UUIDField(default=uuid.uuid4, editable=True, db_index=True),
+            preserve_default=False,
+        ),
+        migrations.RunPython(assign_transaction_uuids, migrations.RunPython.noop),
+    ]
