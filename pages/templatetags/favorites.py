b7e1e7ea60e0393984a9b44c11390b5254156190
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-09-12 18:48:05 -0600

    Ensure favorites star shows without content type

--- pages/templatetags/favorites.py
+++ pages/templatetags/favorites.py
@@ -1,4 +1,5 @@
 from django import template
+from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
 from pages.models import Favorite
 
@@ -8,10 +9,11 @@ register = template.Library()
 @register.simple_tag
 def favorite_ct_id(app_label, model_name):
     try:
-        ct = ContentType.objects.get_by_natural_key(app_label, model_name.lower())
-        return ct.id
-    except ContentType.DoesNotExist:
+        model = apps.get_model(app_label, model_name)
+    except LookupError:
         return None
+    ct = ContentType.objects.get_for_model(model)
+    return ct.id
 
 
 @register.simple_tag(takes_context=True)
