d67ecc4357f655b6960b8d79c364e5b994799eab
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-26 11:47:25 -0600

    Fix site template migration for Site model (#3367)

--- pages/migrations/0048_site_template_on_site.py
+++ pages/migrations/0048_site_template_on_site.py
@@ -1,6 +1,25 @@
 from django.db import migrations, models
 
 
+class AddFieldToSites(migrations.AddField):
+    """Add a field to the ``django.contrib.sites`` ``Site`` model.
+
+    ``AddField`` uses the migration's ``app_label`` (``pages`` here) for both
+    state and database operations. We override the relevant methods so the
+    operation targets the ``sites`` app instead, keeping the migration graph
+    consistent for subsequent migrations that rely on the new field.
+    """
+
+    def state_forwards(self, app_label, state):
+        super().state_forwards("sites", state)
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        super().database_forwards("sites", schema_editor, from_state, to_state)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        super().database_backwards("sites", schema_editor, from_state, to_state)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -9,7 +28,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
+        AddFieldToSites(
             model_name="site",
             name="template",
             field=models.ForeignKey(
