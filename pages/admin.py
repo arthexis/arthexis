703ac6e0ea8238dcce18a0442904607a8bbde98c
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-26 16:19:33 -0600

    Add color picker form to SiteTemplate admin (#3378)
    * Add admin form for site templates
    
    * Handle shorthand colors in SiteTemplate admin form

--- pages/admin.py
+++ pages/admin.py
@@ -321,6 +321,38 @@ admin.site.register(SiteProxy, SiteAdmin)
 
 @admin.register(SiteTemplate)
 class SiteTemplateAdmin(EntityModelAdmin):
+    class SiteTemplateAdminForm(forms.ModelForm):
+        color_fields = (
+            "primary_color",
+            "primary_color_emphasis",
+            "accent_color",
+            "accent_color_emphasis",
+            "support_color",
+            "support_color_emphasis",
+            "support_text_color",
+        )
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name in self.color_fields:
+                value = self.initial.get(field_name) or getattr(self.instance, field_name, None)
+                if isinstance(value, str) and len(value) == 4:
+                    # Preserve 3-digit shorthand colors by using a text widget that accepts #rgb.
+                    self.fields[field_name].widget = forms.TextInput(attrs={"type": "text"})
+
+        class Meta:
+            model = SiteTemplate
+            fields = "__all__"
+            widgets = {
+                "primary_color": forms.TextInput(attrs={"type": "color"}),
+                "primary_color_emphasis": forms.TextInput(attrs={"type": "color"}),
+                "accent_color": forms.TextInput(attrs={"type": "color"}),
+                "accent_color_emphasis": forms.TextInput(attrs={"type": "color"}),
+                "support_color": forms.TextInput(attrs={"type": "color"}),
+                "support_color_emphasis": forms.TextInput(attrs={"type": "color"}),
+                "support_text_color": forms.TextInput(attrs={"type": "color"}),
+            }
+
     list_display = (
         "name",
         "palette",
@@ -328,6 +360,7 @@ class SiteTemplateAdmin(EntityModelAdmin):
         "accent_color",
         "support_color",
     )
+    form = SiteTemplateAdminForm
     search_fields = ("name",)
     fieldsets = (
         (
