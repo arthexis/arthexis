f0b4d1513b9d9d657c084d38153e95c32b509406
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-26 16:09:05 -0600

    Handle shorthand colors in SiteTemplate admin form

--- pages/admin.py
+++ pages/admin.py
@@ -322,6 +322,24 @@ admin.site.register(SiteProxy, SiteAdmin)
 @admin.register(SiteTemplate)
 class SiteTemplateAdmin(EntityModelAdmin):
     class SiteTemplateAdminForm(forms.ModelForm):
+        color_fields = (
+            "primary_color",
+            "primary_color_emphasis",
+            "accent_color",
+            "accent_color_emphasis",
+            "support_color",
+            "support_color_emphasis",
+            "support_text_color",
+        )
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name in self.color_fields:
+                value = self.initial.get(field_name) or getattr(self.instance, field_name, None)
+                if isinstance(value, str) and len(value) == 4:
+                    # Preserve 3-digit shorthand colors by using a text widget that accepts #rgb.
+                    self.fields[field_name].widget = forms.TextInput(attrs={"type": "text"})
+
         class Meta:
             model = SiteTemplate
             fields = "__all__"
