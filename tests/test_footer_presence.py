a8336736d7516be26b0292d84264f22358433c52
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-29 16:34:06 -0600

    Prep 0.2.0 cleanup (#3556)

--- tests/test_footer_presence.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-
-class FooterPresenceTests(TestCase):
-    def test_home_page_includes_footer(self):
-        response = self.client.get("/")
-        self.assertContains(response, "<footer", html=False)
-        self.assertContains(response, "arthexis", html=False)
