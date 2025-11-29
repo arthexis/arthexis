552fad54083fda7ce79ebe60087c34adac706d78
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-29 16:58:19 -0600

    Add stub core migration for celery beat dependency

---
+++ core/migrations/0078_todo_done_node_todo_done_revision_todo_done_username_and_more.py
@@ -0,0 +1,10 @@
+# Generated to satisfy external migration dependency.
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0002_initial"),
+    ]
+
+    operations = []
