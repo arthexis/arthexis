077a6d7b2a6f4cff6aaaf4e3ffec66dbec2523c7
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-29 17:29:48 -0600

    Add stub migration for countdown timer dependency (#3561)

---
+++ core/migrations/0102_countdowntimer.py
@@ -0,0 +1,10 @@
+# Generated to satisfy external migration dependency.
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0078_todo_done_node_todo_done_revision_todo_done_username_and_more"),
+    ]
+
+    operations = []
