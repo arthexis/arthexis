419c5f5aedadea7cd304e7ab67077600b55e1e34
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-11-30 10:38:29 -0600

    Add AWG calculation endpoint with template support (#3610)

--- apps/awg/urls.py
+++ apps/awg/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "awg"
 
 urlpatterns = [
+    path("calculate/", views.awg_calculate, name="awg_calculate"),
     path("", views.calculator, name="calculator"),
     path("future-event/", views.future_event_calculator, name="future_event"),
     path("zapped/", views.zapped_result, name="zapped"),
