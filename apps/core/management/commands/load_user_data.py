333ddd50e65ceee66a5e7f8f002bcc949e5c090e
gelectriic <tecnologia@gelectriic.com>
2025-12-01 10:33:55 -0600

    Remove admin email mailbox provisioning (#3673)

--- apps/core/management/commands/load_user_data.py
+++ apps/core/management/commands/load_user_data.py
@@ -1,19 +1,8 @@
-import logging
-
 from django.core.management.commands.loaddata import Command as DjangoLoadDataCommand
-
-logger = logging.getLogger(__name__)
 
 
 class Command(DjangoLoadDataCommand):
-    """Load user fixtures then ensure admin mailboxes exist."""
+    """Load user fixtures."""
 
     def handle(self, *fixture_labels, **options):
-        result = super().handle(*fixture_labels, **options)
-        try:
-            from apps.emails.models import ensure_admin_email_mailboxes
-
-            ensure_admin_email_mailboxes()
-        except Exception:  # pragma: no cover - defensive logging
-            logger.exception("Unable to ensure admin email mailboxes after load_user_data")
-        return result
+        return super().handle(*fixture_labels, **options)
