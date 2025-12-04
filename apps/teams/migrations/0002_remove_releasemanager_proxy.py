064e167350cd8e151d55da455d2ae188e1aff995
gelectriic <tecnologia@gelectriic.com>
2025-12-04 16:45:36 -0600

    Remove ReleaseManager proxy from teams app

---
+++ apps/teams/migrations/0002_remove_releasemanager_proxy.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("teams", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.DeleteModel(
+            name="ReleaseManager",
+        ),
+    ]
