6b31d1e39887e817baf186bf6c8a4e0069aa1eda
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-06 07:55:26 -0600

    Fix OCPP admin import errors (#3887)

--- apps/ocpp/views/__init__.py
+++ apps/ocpp/views/__init__.py
@@ -1,5 +1,6 @@
 from .actions import dispatch_action
 from .charger_api import charger_detail, charger_list
+from .common import _charger_state, _live_sessions
 from .dashboard import dashboard
 from .public import charger_log_page, charger_page, charger_session_search, charger_status
 from .simulator import cp_simulator
