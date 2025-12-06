51132f07feb54036835ba0cdcb34502c0a59d10b
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-06 16:16:06 -0600

    Remove Google Calendar profile

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -3,11 +3,9 @@
 from .user import User
 from .profile import Profile
 from .passkey_credential import PasskeyCredential
-from .google_calendar_profile import GoogleCalendarProfile
 from .user_phone_number import UserPhoneNumber
 
 __all__ = [
-    "GoogleCalendarProfile",
     "PasskeyCredential",
     "Profile",
     "User",
