5af2e41e9df2546528f004b4aef8cc3d5f0c2ed7
avon-ross <arthexis@proton.me>
2025-12-05 19:25:13 -0600

    Refactor ocpp views into package

---
+++ apps/ocpp/views/__init__.py
@@ -0,0 +1,6 @@
+from .actions import dispatch_action
+from .charger_api import charger_detail, charger_list
+from .dashboard import dashboard
+from .public import charger_log_page, charger_page, charger_session_search, charger_status
+from .simulator import cp_simulator
+from .firmware import firmware_download
