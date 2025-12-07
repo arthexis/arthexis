95caeef204681986708382dd833994e5b4ec1a5e
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-06 19:58:13 -0600

    Fix public site traffic widget template path

--- apps/pages/widgets.py
+++ apps/pages/widgets.py
@@ -10,7 +10,7 @@ from apps.widgets.models import WidgetZone
     slug="public-site-traffic",
     name=_("Public site traffic"),
     zone=WidgetZone.ZONE_SIDEBAR,
-    template_name="pages/widgets/public_site_traffic.html",
+    template_name="widgets/public_site_traffic.html",
     description=_("Recent public site visits"),
 )
 def public_site_traffic_widget(**_kwargs):
