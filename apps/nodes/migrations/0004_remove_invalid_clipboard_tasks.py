b1a4507af1bddcaea5136420da8507e0acd07d1e
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-11 08:07:02 -0600

    Handle legacy clipboard tasks

---
+++ apps/nodes/migrations/0004_remove_invalid_clipboard_tasks.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+
+
+def remove_invalid_clipboard_tasks(apps, schema_editor):
+    try:
+        PeriodicTask = apps.get_model("django_celery_beat", "PeriodicTask")
+    except LookupError:
+        return
+
+    conditions = models.Q(task="nodes.tasks.sample_clipboard") | models.Q(
+        name__icontains="poll-clipboard"
+    )
+    PeriodicTask.objects.filter(conditions).delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("nodes", "0003_platform"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            remove_invalid_clipboard_tasks, reverse_code=migrations.RunPython.noop
+        ),
+    ]
