b9883a9421d417ea0782753ebcb94e7fb9cbb4f2
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-11 14:38:53 -0600

    Remove legacy ocpp views module

--- apps/ocpp/views/__init__.py
+++ apps/ocpp/views/__init__.py
@@ -1,6 +1,6 @@
 from .actions import dispatch_action
 from .charger_api import charger_detail, charger_list
-from .common import _charger_state, _live_sessions
+from .common import _aggregate_dashboard_state, _charger_state, _live_sessions
 from .dashboard import dashboard
 from .public import charger_log_page, charger_page, charger_session_search, charger_status
 from .simulator import cp_simulator
