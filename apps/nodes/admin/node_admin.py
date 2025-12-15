7052792b10cb7b390d069f4e1d644e24635e578d
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-14 20:55:15 -0600

    Add proxy flow for visitor registration (#4158)

--- apps/nodes/admin/node_admin.py
+++ apps/nodes/admin/node_admin.py
@@ -1122,6 +1122,7 @@ class NodeAdmin(SaveBeforeChangeAction, EntityModelAdmin):
             "info_url": reverse("node-info"),
             "register_url": reverse("register-node"),
             "telemetry_url": reverse("register-telemetry"),
+            "visitor_proxy_url": reverse("register-visitor-proxy"),
             "visitor_info_url": visitor_info_url,
             "visitor_register_url": visitor_register_url,
             "visitor_error": visitor_error,
