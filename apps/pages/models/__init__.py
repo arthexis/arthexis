634c75359506653ff62d3b22e9c42515cd4e2afa
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-15 14:58:01 -0600

    Fix SiteProxy admin change list initialization (#4179)

--- apps/pages/models/__init__.py
+++ apps/pages/models/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from ..site_config import ensure_site_fields
+ensure_site_fields()
+
 from .landing import Landing, LandingManager
 from .landing_lead import LandingLead
 from .site_badge import SiteBadge
@@ -8,8 +10,6 @@ from .site_proxy import SiteProxy
 from .site_template import SiteTemplate, SiteTemplateManager
 from .user_story import UserStory
 from .view_history import ViewHistory
-
-ensure_site_fields()
 
 # Import signal handlers.
 from . import signals  # noqa: E402,F401
