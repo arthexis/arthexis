e5aef49ebc64b628d834f4d04d9a75b40b92a0cf
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-15 23:05:47 -0600

    Ensure nodes admin URLs are imported

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import include, path
+from apps.nodes import admin as nodes_admin  # noqa: F401 - ensure custom admin URLs are registered
 from apps.tasks import admin as tasks_admin  # noqa: F401
 from apps.teams import admin as teams_admin  # noqa: F401
 from django.views.decorators.csrf import csrf_exempt
