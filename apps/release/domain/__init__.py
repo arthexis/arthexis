88713dd8a7961ad721abd6282f1e3839cb090447
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-12-31 18:38:59 -0600

    Add release features domain models (#4495)

--- apps/release/domain/__init__.py
+++ apps/release/domain/__init__.py
@@ -1,5 +1,11 @@
 """Release domain helpers for orchestrating release workflows."""
 
+from .features import ReleaseFeature, ReleaseFeatures
 from .release_tasks import capture_migration_state, prepare_release
 
-__all__ = ["capture_migration_state", "prepare_release"]
+__all__ = [
+    "ReleaseFeature",
+    "ReleaseFeatures",
+    "capture_migration_state",
+    "prepare_release",
+]
