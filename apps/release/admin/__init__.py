091faf74844524b070f1ad53b0475887d499335f
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-01-23 17:49:30 -0600

    Update package release action labels

--- apps/release/admin/__init__.py
+++ apps/release/admin/__init__.py
@@ -182,8 +182,8 @@ class PackageReleaseAdmin(SaveBeforeChangeAction, EntityModelAdmin):
             self.message_user(request, message, messages.SUCCESS)
 
     refresh_from_pypi.requires_queryset = False
-    refresh_from_pypi.label = "Refresh from PyPI"
-    refresh_from_pypi.short_description = "Refresh from PyPI"
+    refresh_from_pypi.label = "Sync to PyPI"
+    refresh_from_pypi.short_description = "Sync to PyPI"
 
     def refresh_from_pypi_view(self, request):
         if not self.has_change_permission(request):
@@ -221,8 +221,8 @@ class PackageReleaseAdmin(SaveBeforeChangeAction, EntityModelAdmin):
             return redirect("admin:release_packagerelease_changelist")
         return prepare_package_release(self, request, package)
 
-    prepare_next.label = "Prepare next Release"
-    prepare_next.short_description = "Prepare next release"
+    prepare_next.label = "Prepare next"
+    prepare_next.short_description = "Prepare next"
 
     def _publish_release(self, request, release):
         try:
