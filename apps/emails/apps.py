ace4cb273d8d8080858977573d7ca449f64cbe68
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-01-24 17:06:13 -0600

    Invalidate email dashboard rule cache on profile changes

--- apps/emails/apps.py
+++ apps/emails/apps.py
@@ -5,3 +5,6 @@ class EmailsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.emails"
     verbose_name = "Emails"
+
+    def ready(self) -> None:
+        from . import signals  # noqa: F401
