74484cdb3541099793f51143db6d67ed956f25f9
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-02-01 21:38:45 -0600

    Harden short URL creation

--- apps/links/context_processors.py
+++ apps/links/context_processors.py
@@ -1,4 +1,4 @@
-from django.db.utils import OperationalError, ProgrammingError
+from django.db.utils import DatabaseError
 
 from .models import get_or_create_short_url
 
@@ -7,9 +7,9 @@ def share_short_url(request):
     if request is None:
         return {"share_short_url": ""}
     try:
-        target_url = request.build_absolute_uri()
+        target_url = request.build_absolute_uri(request.path)
         short_url = get_or_create_short_url(target_url)
-    except (OperationalError, ProgrammingError):
+    except DatabaseError:
         return {"share_short_url": ""}
     if not short_url:
         return {"share_short_url": ""}
