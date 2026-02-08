1180a2902a21c01668344c29138016b7444bcac4
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-02-07 21:41:30 -0600

    Add dashboard rule for default CP simulator

--- apps/ocpp/signals.py
+++ apps/ocpp/signals.py
@@ -1,10 +1,11 @@
 import logging
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
+from apps.counters.models import DashboardRule
 from apps.nodes.models import Node, NodeFeature, NodeFeatureAssignment
 
-from .models import Charger
+from .models import Charger, Simulator
 
 logger = logging.getLogger(__name__)
 
@@ -23,3 +24,10 @@ def enable_charge_point_feature(sender, instance, created, **kwargs):
         logger.debug("Charge point feature %s missing; skipping", CHARGE_POINT_FEATURE_SLUG)
         return
     NodeFeatureAssignment.objects.update_or_create(node=node, feature=feature)
+
+
+@receiver([post_save, post_delete], sender=Simulator)
+def invalidate_simulator_dashboard_rule_cache(sender, **_kwargs) -> None:
+    """Invalidate dashboard rule cache for CP simulator default checks."""
+
+    DashboardRule.invalidate_model_cache(sender)
