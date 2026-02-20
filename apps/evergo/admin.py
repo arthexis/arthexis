3a82547107bab77e4c5b00c9fade86562e11c5f1
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-02-20 17:13:22 -0600

    Improve Evergo admin presentation and add admin accessibility tests

--- apps/evergo/admin.py
+++ apps/evergo/admin.py
@@ -25,8 +25,78 @@ class EvergoUserAdmin(OwnableAdminMixin, admin.ModelAdmin):
     )
     search_fields = ("name", "email", "evergo_email", "evergo_user_id")
     list_filter = ("two_fa_enabled", "two_fa_authenticated", "created_at", "updated_at")
-    readonly_fields = ("created_at", "updated_at", "last_login_test_at")
+    readonly_fields = (
+        "evergo_user_id",
+        "name",
+        "email",
+        "empresa_id",
+        "empresa_name",
+        "subempresa_id",
+        "subempresa_name",
+        "two_fa_enabled",
+        "two_fa_authenticated",
+        "two_factor_secret",
+        "two_factor_recovery_codes",
+        "two_factor_confirmed_at",
+        "evergo_created_at",
+        "evergo_updated_at",
+        "last_login_test_at",
+        "created_at",
+        "updated_at",
+    )
     actions = ("test_login_and_sync",)
+    fieldsets = (
+        (
+            "Ownership",
+            {
+                "fields": ("user", "group", "avatar"),
+            },
+        ),
+        (
+            "Credentials",
+            {
+                "fields": ("evergo_email", "evergo_password"),
+            },
+        ),
+        (
+            "Evergo synced profile",
+            {
+                "fields": (
+                    "evergo_user_id",
+                    "name",
+                    "email",
+                    "empresa_id",
+                    "empresa_name",
+                    "subempresa_id",
+                    "subempresa_name",
+                ),
+            },
+        ),
+        (
+            "Two-factor",
+            {
+                "fields": (
+                    "two_fa_enabled",
+                    "two_fa_authenticated",
+                    "two_factor_confirmed_at",
+                    "two_factor_secret",
+                    "two_factor_recovery_codes",
+                ),
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "evergo_created_at",
+                    "evergo_updated_at",
+                    "last_login_test_at",
+                    "created_at",
+                    "updated_at",
+                ),
+            },
+        ),
+    )
 
     @admin.action(description="Test Evergo login and sync profile fields")
     def test_login_and_sync(self, request, queryset):
