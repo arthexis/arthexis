fac024b9f5d391cb8ad3adfd09798bb8b3e6f201
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-03-09 08:50:24 -0600

    Restore apps.locals.user_data helper re-exports

--- apps/locals/user_data/__init__.py
+++ apps/locals/user_data/__init__.py
@@ -1,1 +1,10 @@
-"""Local user data package."""
+"""Backward-compatible imports for local user data helpers."""
+
+from apps.locals.seeds import load_local_seed_zips
+from apps.locals.user_fixtures import load_shared_user_fixtures, load_user_fixtures
+
+__all__ = [
+    "load_local_seed_zips",
+    "load_shared_user_fixtures",
+    "load_user_fixtures",
+]
