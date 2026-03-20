3feaedbb12fb1bb05a6f60f340cddb66c9727275
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-03-20 07:51:54 -0600

    bluetooth remove obsolete app

--- apps/bluetooth/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BluetoothConfig(AppConfig):
-    """Application configuration for Bluetooth inventory and control."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.bluetooth"
