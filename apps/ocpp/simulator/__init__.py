063d227724d8009f2123504502e0265ad594db7e
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-03-21 17:48:28 -0600

    ocpp-simulators-remove-legacy-wrapper

--- apps/ocpp/simulator/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible wrapper for the charge point simulator."""
-
-from apps.simulators import ChargePointSimulator, SimulatorConfig
-
-__all__ = ["ChargePointSimulator", "SimulatorConfig"]
