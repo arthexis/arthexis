6cce4e06a89eb84f8e92ff348e75eb6b79f453ae
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-03-21 18:01:10 -0600

    core: fail fast when fitbit cleanup was skipped

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -13,6 +13,8 @@ class CoreConfig(AppConfig):
     label = "core"
 
     def ready(self):  # pragma: no cover - called by Django
+        from . import checks  # noqa: F401
+
         _setup_celery_beat_integrations()
         _register_admin_and_post_migrate_handlers(self)
         _patch_entity_deserialization()
