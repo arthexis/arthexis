a03b890022cbeee9295ef5df9a7aadd33c237724
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-03-26 16:40:28 -0600

    tests: delete 50 low-value test cases

--- tests/test_settings_helpers.py
+++
@@ -1,7 +0,0 @@
-"""Tests for host validation helpers used by security settings."""
-
-from config.settings_helpers import normalize_forwarded_host_header
-
-
-def test_normalize_forwarded_host_header_uses_first_proxy_value():
-    assert normalize_forwarded_host_header("10.42.0.1, proxy.example") == "10.42.0.1"
