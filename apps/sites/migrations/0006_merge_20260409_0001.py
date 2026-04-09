45933b5f9909dd143377f992d229749fc6afec02
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-04-09 08:31:48 -0600

    Fix nodes/pages migration conflict graph

---
+++ apps/sites/migrations/0006_merge_20260409_0001.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("pages", "0002_initial"),
+        ("pages", "0003_initial"),
+        ("pages", "0004_initial"),
+        ("pages", "0005_siteprofile"),
+    ]
+
+    operations = []
