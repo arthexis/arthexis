bd0300568db1a44779f690105660ecc14a796957
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-04-16 08:25:39 -0600

    ocpp-admin remove miscellaneous shim module

--- apps/ocpp/admin/__init__.py
+++ apps/ocpp/admin/__init__.py
@@ -1,4 +1,9 @@
-from .miscellaneous import registry as _miscellaneous_registry
+from .miscellaneous.registry import certificates_admin as _certificates_admin  # noqa: F401
+from .miscellaneous.registry import core_admin as _core_admin  # noqa: F401
+from .miscellaneous.registry import firmware_admin as _firmware_admin  # noqa: F401
+from .miscellaneous.registry import network_profiles_admin as _network_profiles_admin  # noqa: F401
+from .miscellaneous.registry import simulator_admin as _simulator_admin  # noqa: F401
+from .miscellaneous.registry import transactions_admin as _transactions_admin  # noqa: F401
 from .charge_point import *
 from .monitoring import *
 from .public_pages import *
