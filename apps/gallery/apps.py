23993d8b10b6d3fc92cd8c0047cdf127cbda758e
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2026-04-22 18:33:46 -0600

    gallery: seed default taxonomy categories (#7286)

--- apps/gallery/apps.py
+++ apps/gallery/apps.py
@@ -1,7 +1,17 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class GalleryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.gallery"
     verbose_name = "Gallery"
+
+    def ready(self) -> None:
+        from .defaults import ensure_default_gallery_categories
+
+        post_migrate.connect(
+            ensure_default_gallery_categories,
+            sender=self,
+            dispatch_uid="gallery.default_categories_post_migrate",
+        )
