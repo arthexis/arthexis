c95580ff5d4f25f20ff9c14420d4ad581388c7ac
arthexis <arthexis@gmail.com>
2026-09-04 13:08:08 -0600

    Export canonical OCPP version type

--- apps/ocpp/consumers/__init__.py
+++ apps/ocpp/consumers/__init__.py
@@ -1,9 +1,10 @@
+from .constants import OCPP_VERSION_16, OCPP_VERSION_201, OCPP_VERSION_21, OCPPVersion
 from .csms import CSMSConsumer, SinkConsumer
-from .constants import OCPP_VERSION_16, OCPP_VERSION_201, OCPP_VERSION_21
 
 __all__ = [
     "CSMSConsumer",
     "SinkConsumer",
+    "OCPPVersion",
     "OCPP_VERSION_16",
     "OCPP_VERSION_201",
     "OCPP_VERSION_21",
