5dc97f1d717aff062979354d0057036022076111
arthexis <arthexis@gmail.com>
2026-09-04 19:14:52 -0600

    imager: define Debian-based Pi default image

--- apps/imager/constants.py
+++ apps/imager/constants.py
@@ -1,6 +1,8 @@
 """Shared constants for image artifact workflows."""
 
 DEFAULT_ARTHEXIS_GIT_URL = ""
+DEFAULT_RPI4B_BASE_IMAGE_URI = "https://downloads.raspberrypi.com/raspios_lite_arm64_latest"
+DEFAULT_RPI4B_BASE_IMAGE_DESCRIPTION = "Raspberry Pi OS Lite (Debian, arm64)"
 UNIVERSAL_CONNECT_UPDATE_ROLES = ("Terminal", "Satellite", "Control", "Watchtower")
 UNIVERSAL_CONNECT_UPDATE_REQUIRED_ARTIFACTS = (
     "connect-ota-agent",
