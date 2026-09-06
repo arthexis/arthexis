186f78ccc608a106819a5e231d59ff696eb7e952
arthexis <arthexis@gmail.com>
2026-09-05 21:40:56 -0600

    refactor: use email-owned mailbox admin classes

--- apps/emails/admin.py
+++ apps/emails/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.db.models import Max
 from django.utils.translation import gettext_lazy as _
 
-from apps.core.admin import EmailCollectorAdmin, EmailInboxAdmin
 from apps.core.admin.metrics import annotate_enabled_total, format_enabled_total, max_attr
 from apps.locals.user_data import EntityModelAdmin
 
+from .admin_impl.emails import EmailCollectorAdmin, EmailInboxAdmin
 from .admin_outbox import EmailOutboxAdmin
 from .models import EmailBridge, EmailCollector, EmailInbox, EmailOutbox
 
