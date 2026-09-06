2a0100c44c47465b014a34e5d1379487b096d22d
arthexis <arthexis@gmail.com>
2026-09-05 23:28:25 -0600

    refactor: register release task compatibility aliases

--- apps/release/apps.py
+++ apps/release/apps.py
@@ -5,3 +5,6 @@ class ReleaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.release"
     verbose_name = "Releases"
+
+    def ready(self):
+        from apps.release import task_compat  # noqa: F401
