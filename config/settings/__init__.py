09d8dfff853f4849739efb74457c4a17eea1a995
arthexis <arthexis@gmail.com>
2026-09-08 19:10:54 -0600

    Keep Celery settings out of Terminal role

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,12 +1,25 @@
 """Assembled Django settings package."""
 
 from config.bootstrap import bootstrap_django_environment
+from config.roles import role_requires_shared_channel_layer
 
 bootstrap_django_environment()
 
 from .base import *  # noqa: F401,F403,E402
 from .security import *  # noqa: F401,F403,E402
 from .apps import *  # noqa: F401,F403,E402
+
+CELERY_RUNTIME_ENABLED = role_requires_shared_channel_layer(NODE_ROLE)
+if not CELERY_RUNTIME_ENABLED:
+    _CELERY_APP_ENTRIES = {
+        "apps.celery",
+        "apps.celery.beat_app.CeleryBeatConfig",
+    }
+    PROJECT_LOCAL_APPS[:] = [
+        app for app in PROJECT_LOCAL_APPS if app not in _CELERY_APP_ENTRIES
+    ]
+    PROJECT_APPS[:] = [app for app in PROJECT_APPS if app not in _CELERY_APP_ENTRIES]
+    INSTALLED_APPS[:] = [app for app in INSTALLED_APPS if app not in _CELERY_APP_ENTRIES]
 
 # Events is a local Arthexis app and may be enabled by the install app lock.
 # Keep the assembled registry aware of it until the app registry is consolidated.
@@ -34,7 +47,9 @@ from .database import *  # noqa: F401,F403,E402
 from .i18n import *  # noqa: F401,F403,E402
 from .static import *  # noqa: F401,F403,E402
 from .logging import *  # noqa: F401,F403,E402
-from .celery import *  # noqa: F401,F403,E402
+
+if CELERY_RUNTIME_ENABLED:
+    from .celery import *  # noqa: F401,F403,E402
 
 from config.roles import validate_role_settings  # noqa: E402
 
