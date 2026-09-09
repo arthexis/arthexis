da40af022f9f17fc3e2f3dd02d87f1479c9fcf3e
arthexis <arthexis@gmail.com>
2026-09-08 18:05:44 -0600

    Resolve channel warning severity by node role

--- config/settings/channels.py
+++ config/settings/channels.py
@@ -3,6 +3,7 @@
 import os
 
 from config.channel_layer import resolve_channel_layers
+from config.roles import role_requires_shared_channel_layer
 from utils.env import env_bool
 
 from .broker import resolve_celery_broker_url
@@ -27,6 +28,9 @@ OCPP_AUTHORIZATION_POLICY = (
 CHANNEL_LAYERS, CHANNEL_LAYER_DECISION = resolve_channel_layers(
     channel_redis_url=CHANNEL_REDIS_URL,
     ocpp_state_redis_url=OCPP_STATE_REDIS_URL,
+    shared_backend_required=role_requires_shared_channel_layer(
+        os.environ.get("NODE_ROLE", "Terminal")
+    ),
 )
 
 OCPP_PENDING_CALL_TTL = int(os.environ.get("OCPP_PENDING_CALL_TTL", "1800"))
