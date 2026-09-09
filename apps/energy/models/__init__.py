1bc45e528441653cc58222216c67dc91a8ca333e
arthexis <arthexis@gmail.com>
2026-09-08 20:02:58 -0600

    Restore energy reporting model exports

--- apps/energy/models/__init__.py
+++ apps/energy/models/__init__.py
@@ -1,7 +1,7 @@
-from django.conf import settings
-
 from .billing import CustomerAccount, EnergyTariff, EnergyTariffManager, Location
 from .transactions import EnergyTransaction, generate_missing_reports
+from .scheduling import ClientReportSchedule
+from .reporting import ClientReport
 
 __all__ = [
     "CustomerAccount",
@@ -9,24 +9,7 @@ __all__ = [
     "EnergyTariffManager",
     "Location",
     "EnergyTransaction",
+    "ClientReportSchedule",
+    "ClientReport",
     "generate_missing_reports",
 ]
-
-if getattr(settings, "CELERY_RUNTIME_ENABLED", True):
-    from .reporting import ClientReport
-    from .scheduling import ClientReportSchedule
-
-    __all__.extend(["ClientReportSchedule", "ClientReport"])
-else:
-
-    class ClientReport:
-        """Terminal-safe facade for Celery-backed reporting."""
-
-        @staticmethod
-        def build_rows(*args, **kwargs):
-            raise RuntimeError(
-                "Client reporting is unavailable on Terminal nodes; "
-                "upgrade this node to Control, Satellite, or Watchtower to enable it."
-            )
-
-    __all__.append("ClientReport")
