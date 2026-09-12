3be4c844000dc8220b122a84a8391ecf089ff049
arthexis <arthexis@gmail.com>
2026-09-11 20:05:53 -0600

    compat: backfill Python 3.11 stdlib APIs on 3.10

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 import datetime
 import enum
+import sys
+import typing
 
 
 if not hasattr(enum, "StrEnum"):
@@ -15,6 +17,29 @@ if not hasattr(enum, "StrEnum"):
             return str(self.value)
 
     enum.StrEnum = StrEnum  # type: ignore[attr-defined]
+else:
+    StrEnum = enum.StrEnum
 
 if not hasattr(datetime, "UTC"):
     datetime.UTC = datetime.timezone.utc  # type: ignore[attr-defined]
+
+if sys.version_info < (3, 11):
+    import tomli
+    import typing_extensions
+
+    sys.modules.setdefault("tomllib", tomli)
+    for _name in (
+        "LiteralString",
+        "Never",
+        "NotRequired",
+        "Required",
+        "Self",
+        "TypeVarTuple",
+        "Unpack",
+        "assert_never",
+        "assert_type",
+        "dataclass_transform",
+        "reveal_type",
+    ):
+        if not hasattr(typing, _name):
+            setattr(typing, _name, getattr(typing_extensions, _name))
