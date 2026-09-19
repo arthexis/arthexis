5a5e6e702c270b72cc0f17499de86fe1cbc61c37
arthexis <arthexis@gmail.com>
2026-09-19 17:19:11 -0600

    ci: fix refactor lint failures

--- tests/ocpp/test_snapshots.py
+++ tests/ocpp/test_snapshots.py
@@ -4,7 +4,13 @@ from decimal import Decimal
 from django.test import TestCase
 
 from apps.ocpp.domain.snapshots import snapshot_charger, snapshot_chargers
-from tests.ocpp.builders import charger, connection, connector, station_model, transaction
+from tests.ocpp.builders import (
+    charger,
+    connection,
+    connector,
+    station_model,
+    transaction,
+)
 
 
 class ChargerSnapshotTests(TestCase):
