56df4928256cbdbcabfa69f818285dae051aa163
arthexis <arthexis@gmail.com>
2026-09-22 14:03:06 -0600

    E5B: register OCPP event subscribers

--- apps/ocpp/apps.py
+++ apps/ocpp/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class OcppConfig(AppConfig):
     name = "apps.ocpp"
+
+    def ready(self) -> None:
+        from apps.ocpp.subscribers import register_subscribers
+
+        register_subscribers()
