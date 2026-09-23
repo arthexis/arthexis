9d590c1ae703c656eddcb66b367efa3c5a729167
arthexis <arthexis@gmail.com>
2026-09-23 06:53:02 -0600

    Add protocol operation reconciliation provenance

---
+++ apps/ocpp/migrations/0022_protocol_operation_reconciliation.py
@@ -0,0 +1,38 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("ocpp", "0021_ocpp_transaction_meter_derivation_revisions"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="protocoloperation",
+            name="reconciliation_checked_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="protocoloperation",
+            name="reconciled_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="protocoloperation",
+            name="reconciliation_resolution",
+            field=models.CharField(
+                blank=True,
+                choices=[
+                    ("achieved", "Desired state achieved"),
+                    ("not_achieved", "Desired state not achieved"),
+                    ("irrelevant", "No longer operationally relevant"),
+                ],
+                max_length=24,
+            ),
+        ),
+        migrations.AddField(
+            model_name="protocoloperation",
+            name="reconciliation_basis",
+            field=models.CharField(blank=True, max_length=240),
+        ),
+    ]
