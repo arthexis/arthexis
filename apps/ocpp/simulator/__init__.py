5bcef0ab9ffa17d1edad5c4b40a8c00e646307b8
arthexis <arthexis@gmail.com>
2026-09-25 10:22:48 -0600

    Export migrated database replay source

--- apps/ocpp/simulator/__init__.py
+++ apps/ocpp/simulator/__init__.py
@@ -1,6 +1,7 @@
 """In-process OCPP protocol-client scenarios for retained contracts."""
 
 from apps.ocpp.simulator.client import OcppSimulator
+from apps.ocpp.simulator.database_replay import ReplayEvent, iter_v16_transaction_replay
 from apps.ocpp.simulator.scenarios import (
     AuthorizationScenarioResult,
     run_v16_authorization_scenario,
@@ -11,6 +12,8 @@ from apps.ocpp.simulator.scenarios import (
 __all__ = [
     "AuthorizationScenarioResult",
     "OcppSimulator",
+    "ReplayEvent",
+    "iter_v16_transaction_replay",
     "run_v16_authorization_scenario",
     "run_v16_scenario",
     "run_v201_scenario",
