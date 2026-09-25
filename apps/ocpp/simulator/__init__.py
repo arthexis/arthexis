bd5fdd372dc9efdc2eec2f11a800d9602b8a1461
arthexis <arthexis@gmail.com>
2026-09-25 10:17:26 -0600

    Export authorization simulator scenario helpers

--- apps/ocpp/simulator/__init__.py
+++ apps/ocpp/simulator/__init__.py
@@ -1,6 +1,17 @@
 """In-process OCPP protocol-client scenarios for retained contracts."""
 
 from apps.ocpp.simulator.client import OcppSimulator
-from apps.ocpp.simulator.scenarios import run_v16_scenario, run_v201_scenario
+from apps.ocpp.simulator.scenarios import (
+    AuthorizationScenarioResult,
+    run_v16_authorization_scenario,
+    run_v16_scenario,
+    run_v201_scenario,
+)
 
-__all__ = ["OcppSimulator", "run_v16_scenario", "run_v201_scenario"]
+__all__ = [
+    "AuthorizationScenarioResult",
+    "OcppSimulator",
+    "run_v16_authorization_scenario",
+    "run_v16_scenario",
+    "run_v201_scenario",
+]
